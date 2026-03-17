c5c46645d1bae71d649b496f1aaf4c44b025bd06
Rocky <rakshitgame20@gmail.com>
2026-03-17 10:43:35 +0530

    .

---
+++ scripts/coin.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+@onready var game_manager: Node = %GameManager
+
+
+func _on_body_entered(body: Node2D) -> void:
+	game_manager.addPoint()
+	queue_free()
---
+++ scripts/game_manager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var score = 0
+
+func addPoint():
+	score+=1
+	print(score)
